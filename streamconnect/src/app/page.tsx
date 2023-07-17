a5c633ef766c33878c0b7eb93bef29722ca9896a
Sebastian <hawaiilive@outlook.com>
2023-07-17 09:08:56 +0200

    configure prettier

--- streamconnect/src/app/page.tsx
+++ streamconnect/src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <></>;
+    return (
+        <div className="h-full bg-gradient-to-br from-blue-200 to-green-200"></div>
+    );
 }
