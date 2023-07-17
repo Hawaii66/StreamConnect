78cd51d419cad6e6a7d5cfc3f7ad9e63dc99680c
Sebastian <hawaiilive@outlook.com>
2023-07-17 09:11:03 +0200

    test rebase

--- streamconnect/src/app/page.tsx
+++ streamconnect/src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
     return (
-        <div className="h-full bg-gradient-to-br from-blue-200 to-green-200"></div>
+        <div className="h-full bg-gradient-to-br from-blue-200 to-green-200">
+            <h1>What</h1>
+        </div>
     );
 }
